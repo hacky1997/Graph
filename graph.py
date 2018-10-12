d76b0fdd682d37cbcb84cea351e02b985e4c2bfe
hacky1997 <35650583+hacky1997@users.noreply.github.com>
2018-10-12 15:09:51 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+#import matplotlib.mlab as mlab
+#from pandas import DataFrame
+#import pandas.io.data
+
+x = input("enter your x-Axis :")
+y = input("enter your y-Axis :")
+plt.xlabel('x Axis')
+plt.ylabel('y axis')
+plt.plot([x], [y])
+plt.show()
